7b21f869e9812f551bebd811e66519a1f29c67d1
Yamini Rathish <yamini.rathish@gmail.com>
2023-06-20 18:03:09 +0200

    Used props and changed the background value back to white

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
-import './App.css';
-import ColorDisplay from "./colorDisplay.js"
+import { useState } from "react";
+import "./App.css";
+import ColorDisplay from "./colorDisplay.js";
 
 function App() {
+  const [text, setText] = useState("Empty Value");
+  const [textColor, setTextColor] = useState("black");
+  const [hexValue, setHexValue] = useState("");
+  const [bgColor, setBgColor] = useState("white");
   return (
     <div className="App">
-      <ColorDisplay/>
+      <ColorDisplay
+        text={text}
+        setText={setText}
+        textColor={textColor}
+        setTextColor={setTextColor}
+        hexValue={hexValue}
+        setHexValue={setHexValue}
+        bgColor={bgColor}
+        setBgColor={setBgColor}
+      />
     </div>
   );
 }
--- src/colorDisplay.js
+++ src/colorDisplay.js
@@ -1,26 +1,32 @@
-import { useState } from "react";
 import "./colorDisplay.css";
-import colorNames from 'colornames'
+import colorNames from "colornames";
 
-const ColorDisplay = () => {
-  const [text, setText] = useState("Empty Value");
-  const [textColor, setTextColor] = useState("black");
-  const [hexValue, setHexValue] = useState("");
+const ColorDisplay = ({
+  text,
+  setText,
+  textColor,
+  setTextColor,
+  hexValue,
+  setHexValue,
+  bgColor,
+  setBgColor,
+}) => {
 
+    
   const handleTextChange = (e) => {
     const change = e.target.value;
     setText(change === "" ? "Empty Value" : change);
     setHexValue(colorNames(change));
+    setBgColor(change === "" ? "white" : change);
   };
 
   const handleTextColorChange = () => {
     setTextColor((prevColor) => (prevColor === "black" ? "white" : "black"));
   };
 
-
   return (
     <div className="main">
-      <div className="display" style={{ backgroundColor: text}}>
+      <div className="display" style={{ backgroundColor: bgColor }}>
         <p className="innertext" style={{ color: textColor }}>
           {text} <br></br>
           {hexValue}
@@ -32,7 +38,6 @@ const ColorDisplay = () => {
         type="text"
         placeholder="Add Color Name"
         onChange={handleTextChange}
-        
       />
       <button className="button" onClick={handleTextColorChange}>
         Toggle Text Color
